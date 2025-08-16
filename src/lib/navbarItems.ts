7c6b974c865880c6a5b83915162df91dd4680eac
Ali Sanan <alisanan9090@gmail.com>
2025-08-16 22:22:13 +0500

    initial commit

---
+++ src/lib/navbarItems.ts
@@ -0,0 +1,7 @@
+const navbarItems: { title: string; href: string }[] = [
+	{ title: "Home", href: "/" },
+	{ title: "About", href: "/about" },
+	{ title: "Contact", href: "/contact" },
+];
+
+export default navbarItems;
