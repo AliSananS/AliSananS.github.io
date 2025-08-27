7cd311baca6e986cc6bdda3dbbea454dcf880781
Ali Sanan <alisanan9090@gmail.com>
2025-08-27 17:13:48 +0500

    add projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,37 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
+
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-	},
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [icon()],
+  server: {
+    host: "0.0.0.0",
+    port: 4000,
+    open: "http://localhost:4000/",
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "inter",
+        cssVariable: "--font-inter",
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Jetbrains Mono",
+        cssVariable: "--font-jetbrains-mono",
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Fira Code",
+        cssVariable: "--font-fira-code",
+      },
+    ],
+  },
 });
---
+++ src/siteConfig.ts
@@ -0,0 +1,12 @@
+const config = {
+	siteTitle: "Ali Sanan",
+	siteDescription: "Ali Sanan's portfolio",
+	socialLinks: {
+		// twitter: "https://twitter.com/yourusername",
+		github: "https://github.com/AliSananS",
+		// linkedin: "https://linkedin.com/in/yourusername"
+	},
+	email: "ali@devshell.blog",
+} as const;
+
+export default config;
