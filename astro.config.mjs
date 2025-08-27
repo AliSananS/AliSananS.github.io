636c518f462dc4a805f4ad0dac336ff20854b928
Ali Sanan <alisanan9090@gmail.com>
2025-08-27 17:18:48 +0500

    Added deply.yml file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://AliSananS.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
