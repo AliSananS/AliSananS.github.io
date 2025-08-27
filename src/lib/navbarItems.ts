a296012643940da28f1fe0c6b13aa339fc4812bc
Ali Sanan <alisanan9090@gmail.com>
2025-08-27 18:06:25 +0500

    fixed background opacity

--- src/lib/navbarItems.ts
+++ src/lib/navbarItems.ts
@@ -1,5 +1,5 @@
 const navbarItems: { title: string; href: string }[] = [
-	{ title: "Home", href: "/" },
+	// { title: "Home", href: "/" },
 	{ title: "About", href: "/about" },
 	{ title: "Contact", href: "/contact" },
 ];
